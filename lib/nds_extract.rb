668d22ebfe9aa35198c2eceb431963083ee84713
Tisdale Fry <tisdalefry@gmail.com>
2020-03-04 15:24:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
-
+require 'pry'
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
 
@@ -48,6 +48,14 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  
+  name_movie_arr = []
+  idx = 0  
+  while idx < movies_collection.length do
+    name_movie_arr << movie_with_director_name(name, movies_collection[idx])
+    idx += 1
+  end
+  name_movie_arr
 end
 
 
@@ -63,9 +71,29 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+  studio_grosses = {}
+  idx = 0
+  
+  while idx < collection.length do
+    jdx = 0
+    while jdx < collection.length do
+      studios = collection[jdx][:studio]
+      total = collection[jdx][:worldwide_gross]
+      if studio = nil
+        studios = total
+      elsif studio != 0
+        studios += total
+      end 
+      studio_grosses[:studios] = total
+      jdx += 1
+    end
+    idx += 1
+  end
+  studio_grosses
 end
 
 def movies_with_directors_set(source)
+  
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
@@ -76,6 +104,18 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  #movie_with_director_name(director_name, movie_data)
+  
+  new_arr = []
+  idx = 0
+  while idx < source.length do
+    director_name = source[idx][:name]
+    director_movies = source[idx][:movies]
+    result = movies_with_director_key(director_name, director_movies)
+    new_arr << result
+    idx += 1
+  end
+  new_arr
 end
 
 # ----------------    End of Your Code Region --------------------
